02a2d56998c6f990b7123d6c05952a7652184337
Bruno Hiago Xavier <94971687+BrunoHiago@users.noreply.github.com>
2024-08-30 23:02:27 -0300

    update

--- Exercicio1.py
+++ Exercicio1.py
@@ -1,9 +1,17 @@
-# Exercício 1
 
-ind = 13
-soma= 0
-k = 0
-while k < ind:
-    k = k + 1
-    soma = soma + k
-print(soma)
+# Exercício 1
+def fibonacciSequence(n):
+    fib_sequence = [0, 1]
+    while fib_sequence[-1] < n:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    return fib_sequence
+
+def checkFibonacci(n):
+    fib_sequence = fibonacciSequence(n)
+    if n in fib_sequence:
+        return f"O número {n} pertence à sequência de Fibonacci."
+    else:
+        return f"O número {n} não pertence à sequência de Fibonacci."
+
+numero = int(input("Digite um número para verificar se pertence à sequência de Fibonacci: "))
+print(checkFibonacci(numero))
--- Exercicio2.py
+++ Exercicio2.py
@@ -1,17 +1,25 @@
-
 # Exercício 2
-def fibonacciSequence(n):
-    fib_sequence = [0, 1]
-    while fib_sequence[-1] < n:
-        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
-    return fib_sequence
 
-def checkFibonacci(n):
-    fib_sequence = fibonacciSequence(n)
-    if n in fib_sequence:
-        return f"O número {n} pertence à sequência de Fibonacci."
-    else:
-        return f"O número {n} não pertence à sequência de Fibonacci."
+name = input("Digite um nome: ")
 
-numero = int(input("Digite um número para verificar se pertence à sequência de Fibonacci: "))
-print(checkFibonacci(numero))
+
+def count_letters_A(name):
+    count = 0
+    contupper = 0
+    contlower = 0
+    for letter in name:
+        
+        if letter == 'A':
+            contupper += 1
+            count += 1
+        if letter == 'a':
+            contlower += 1
+            count += 1
+    return count, contupper, contlower   
+
+count, contupper, contlower = count_letters_A(name)
+
+print(f"O nome {name} possui {count} letras 'A', {contupper} 'A' maiúsculas e {contlower} 'a' minúsculas.")
+
+
+
